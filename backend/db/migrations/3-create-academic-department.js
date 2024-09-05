fe3e2951d86fac377d89e895cde887952f1b7201
Scott Feichter <scottfeichter@gmail.com>
2024-09-04 17:45:51 -0700

    misc (msg via sh alias)

--- backend/db/migrations/3-create-academic-department.js
+++ backend/db/migrations/3-create-academic-department.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      'AcademicDepartments',
+      "AcademicDepartments",
       {
         id: {
           allowNull: false,
@@ -31,19 +31,19 @@ module.exports = {
         createdAt: {
           allowNull: false,
           type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
         updatedAt: {
           allowNull: false,
           type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
       },
       options
     );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'AcademicDepartments';
+    options.tableName = "AcademicDepartments";
     await queryInterface.dropTable(options);
   },
 };
--- backend/db/migrations/8-create-students.js
+++ backend/db/migrations/8-create-students.js
@@ -1,171 +1,172 @@
-'use strict';
+"use strict";
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Students', {
-      id: {
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-        unique: true,
-        type: Sequelize.INTEGER
-      },
-      household_Id: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Households',
-          key: 'id',
+    await queryInterface.createTable(
+      "Students",
+      {
+        id: {
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+          unique: true,
+          type: Sequelize.INTEGER,
         },
-        // onDelete: 'CASCADE'
-      },
-      headOfHousehold: {
-        allowNull: false,
-        unique: false,
-        type: Sequelize.BOOLEAN
-      },
-      level_Id: {
-        allowNull: true,
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Levels',
-          key: 'id',
+        household_Id: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Households",
+            key: "id",
+          },
+          // onDelete: 'CASCADE'
         },
-        // onDelete: 'CASCADE'
+        headOfHousehold: {
+          allowNull: false,
+          unique: false,
+          type: Sequelize.BOOLEAN,
+        },
+        level_Id: {
+          allowNull: true,
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Levels",
+            key: "id",
+          },
+          // onDelete: 'CASCADE'
+        },
+        firstName1: {
+          allowNull: false,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        firstName2: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        middleName: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        lastName1: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        lastName2: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        personalEmail: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        personalPhone: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        street: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        city: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        state: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        zip: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.INTEGER,
+        },
+        dob: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        age: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.INTEGER,
+        },
+        firstLang: {
+          allowNull: false,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        secondLang: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        thirdLang: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        firstInst: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        secondInst: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        thirdInst: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.STRING,
+        },
+        yearlyIncome: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.INTEGER,
+        },
+        biography: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.TEXT,
+        },
+        notes: {
+          allowNull: true,
+          unique: false,
+          type: Sequelize.TEXT,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      firstName1: {
-        allowNull: false,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      firstName2: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      middleName: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      lastName1: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      lastName2: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      personalEmail: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      personalPhone: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      street: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      city: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      state: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      zip: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.INTEGER
-      },
-      dob: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      age: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.INTEGER
-      },
-      firstLang: {
-        allowNull: false,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      secondLang: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      thirdLang: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      firstInst: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      secondInst: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      thirdInst: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.STRING
-      },
-      yearlyIncome: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.INTEGER
-      },
-      biography: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.TEXT
-      },
-      notes: {
-        allowNull: true,
-        unique: false,
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      }
-    },
-    options
-
-  );
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Students';
+    options.tableName = "Students";
     await queryInterface.dropTable(options);
-  }
+  },
 };
