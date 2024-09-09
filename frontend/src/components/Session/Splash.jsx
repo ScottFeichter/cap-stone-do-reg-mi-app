9f8241f3519dc533f1bf1d591cf0d345486f7571
Scott Feichter <scottfeichter@gmail.com>
2024-09-09 00:38:30 -0700

    splash cleanup

--- frontend/src/components/Session/Splash.jsx
+++ frontend/src/components/Session/Splash.jsx
@@ -1,6 +1,6 @@
 import './Splash.css';
 import LoginForm from "./LoginForm/LoginForm.jsx";
-import SignupFormModal from "./SignupForm/SignupFormModal/SignupFormModal.jsx";
+import SignupForm from "./SignupForm/SignupForm.jsx";
 
 
 function SplashPage() {
@@ -16,7 +16,7 @@ function SplashPage() {
 
             <div id="SplashDivLoginSignupContainer">
                 <LoginForm></LoginForm>
-                <SignupFormModal></SignupFormModal>
+                <SignupForm></SignupForm>
             </div>
 
           </main>
