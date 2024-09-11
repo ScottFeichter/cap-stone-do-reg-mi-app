3809e8739c4141a705783491c3ee1c009ca8ba73
Scott Feichter <scottfeichter@gmail.com>
2024-09-10 17:56:24 -0700

    misc (msg via sh alias)

--- frontend/src/components/Employees/EmployeesList/EmployeeListSort/EmployeeListSort.jsx
+++ frontend/src/components/Employees/EmployeesList/EmployeeListSort/EmployeeListSort.jsx
@@ -1,7 +1,7 @@
-import { MdFilterListAlt } from "react-icons/md";
-import { RiFilterFill } from "react-icons/ri";
+// import { MdFilterListAlt } from "react-icons/md";
+// import { RiFilterFill } from "react-icons/ri";
 
-import { BiSolidSortAlt } from "react-icons/bi";
-import { TiArrowUnsorted } from "react-icons/ti";
-import { FaSort } from "react-icons/fa6";
-import { FaSort } from "react-icons/fa";
+// import { BiSolidSortAlt } from "react-icons/bi";
+// import { TiArrowUnsorted } from "react-icons/ti";
+// import { FaSort } from "react-icons/fa6";
+// import { FaSort } from "react-icons/fa";
